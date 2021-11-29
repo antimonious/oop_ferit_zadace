0b49a747776788aafe30757bf63d48b11c13abc4
leotumbas <leotumbas@users.noreply.github.com>
2021-11-29 18:22:46 +0100

    Is this finally working

---
+++ DZ1/Weather library/Weather.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Weather_library
+{
+    public class Weather
+    {
+        private const double c1 = -8.78469475556;
+        private const double c2 = 1.61139411;
+        private const double c3 = 2.33854883889;
+        private const double c4 = -0.14611605;
+        private const double c5 = -0.012308094;
+        private const double c6 = -0.0164248277778;
+        private const double c7 = 0.002211732;
+        private const double c8 = 0.00072546;
+        private const double c9 = -0.000003582;
+        private double temperature;
+        private double humidity;
+        private double windSpeed;
+
+        public Weather(){
+            temperature = 0.0;
+            humidity = 0.0;
+            windSpeed = 0.0;
+        }
+
+        public Weather(double temperature, double humidity, double windSpeed){
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.windSpeed = windSpeed;
+        }
+
+        public void SetTemperature(double temperature) { this.temperature = temperature; }
+        public void SetWindSpeed(double windSpeed) { this.windSpeed = windSpeed; }
+        public void SetHumidity(double humidity) { this.humidity = humidity; }
+
+        public double GetTemperature() { return this.temperature; }
+        public double GetWindSpeed() { return this.windSpeed; }
+        public double GetHumidity() { return this.humidity; }
+
+        public double CalculateFeelsLikeTemperature(){
+            return c1 + c2 * temperature + c3 * humidity + c4 * temperature * humidity +
+                c5 * Math.Pow(temperature, 2) + c6 * Math.Pow(humidity, 2)
+                + c7 * Math.Pow(temperature, 2) * humidity +
+                c8 * temperature * Math.Pow(humidity, 2) +
+                c9 * Math.Pow(temperature, 2) * Math.Pow(humidity, 2);
+        }
+
+        public double CalculateWindChill(){
+            if (temperature <= 10 && windSpeed > 4.8)
+                return 13.12 + 0.6215 * temperature - 11.37 * Math.Pow(windSpeed, 0.16) +
+                    0.3965 * temperature * Math.Pow(windSpeed, 0.16);
+            else return 0;
+        }
+
+        static public Weather FindWeatherWithLargestWindchill(Weather[] weathers){
+            Weather maxWindChillWeather = weathers[0];
+            foreach (Weather tempWeather in weathers)
+                if (tempWeather.CalculateWindChill() > maxWindChillWeather.CalculateWindChill())
+                    maxWindChillWeather = tempWeather;
+            return maxWindChillWeather;
+        }
+    }
+}
